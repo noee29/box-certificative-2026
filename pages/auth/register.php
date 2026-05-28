2b1002aa6afe0c93eb818207235942788de6c4a1
Noé Labbé <noe.labbe29@gmail.com>
2026-05-28 13:41:55 +0100

    Add user feedback message and login action redirect

--- pages/auth/register.php
+++ pages/auth/register.php
@@ -54,7 +54,14 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
             $query = $bdd -> prepare("INSERT INTO users(pseudo, email, password) VALUES (?, ?, ?)");
             $query -> execute($tab);
         }
+
+        $result = "Compte créé avec succès !";
+        echo $result;
         
+    }
+    if ($_POST['action'] == 'login') {
+        header('Location: login.php');
+        exit();
     }
 
 }
