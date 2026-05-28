38c8628a273468aa484bc2d5dc67f6b2fe64f4be
Noé Labbé <noe.labbe29@gmail.com>
2026-05-28 10:28:54 +0100

    changes to the website's home page (index.php)

---
+++ pages/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<html>
+    <body>
+        <h1>Page d'accueil : Planificateur de voyages</h1>
+        <p>Cliquez ci dessous pour vous connecter :</p>
+        <a href="../pages/auth/login.php">Se connecter</a>    
+    </body>
+</html>
