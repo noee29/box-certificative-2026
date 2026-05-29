6f6787c8af67c12ca69543998e746b71377cd2d5
Noé Labbé <noe.labbe29@gmail.com>
2026-05-29 07:00:03 +0100

    add comments on index.php file

--- pages/index.php
+++ pages/index.php
@@ -2,6 +2,8 @@
 
 <html>
     <body>
+        <!-- Home page -->
+        
         <h1>Page d'accueil : Planificateur de voyages</h1>
         <p>Cliquez ci dessous pour vous connecter :</p>
         <a href="../pages/auth/login.php">Se connecter</a>    
